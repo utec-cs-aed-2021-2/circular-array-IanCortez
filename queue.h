4b1f52f997ac77767c36311ef46e97afae464055
NeZx28 <66690631+NeZx28@users.noreply.github.com>
2021-09-12 23:55:28 -0500

    finished queue

--- queue.h
+++ queue.h
@@ -2,5 +2,37 @@
 
 template <typename T>
 class QueueArray : public CircularArray<T> { 
-    
+public:
+	QueueArray();
+	QueueArray(int _capacity);
+	void enqueue(T data);
+	T dequeue();
 };
+
+
+template <typename T>
+QueueArray<T>::QueueArray(): CircularArray<T>() {}
+
+template <typename T>
+QueueArray<T>::QueueArray(int _capacity): CircularArray<T>(_capacity) {}
+
+template <typename T>
+void QueueArray<T>::enqueue(T data){
+	if(back == front == -1){
+		back = front = 0;
+	} else {
+		back = next(back);
+	}
+	array[back] = data;
+}
+
+template <typename T>
+T QueueArray<T>::dequeue(){
+	T temp = array[front];
+	if(front == back){
+		front = back = -1;
+	} else {
+		front = next(front);
+	}
+	return temp;
+}
